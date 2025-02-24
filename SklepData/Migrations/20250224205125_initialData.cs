0a25c138f19544e40b05bcecb0f6cd1f10e28eca
Michał Okroja <michalokroja@gmail.com>
2025-02-24 22:33:51 +0100

    Widoki w portalsklep

---
+++ SklepData/Migrations/20250224205125_initialData.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SklepData.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    IdClient = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientIdNumber = table.Column<int>(type: "int", nullable: false),
+                    ClientName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientSurname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClientEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClientPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClientStatus = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.IdClient);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Factures",
+                columns: table => new
+                {
+                    IdFacture = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NrFacture = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateFacture = table.Column<DateOnly>(type: "date", nullable: false),
+                    DateSale = table.Column<DateOnly>(type: "date", nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientNdName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientNdSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientAdressCity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientAdressStreetNr = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientContact = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PriceNetto = table.Column<SqlMoney>(type: "money", nullable: false),
+                    PriceVAT = table.Column<SqlMoney>(type: "money", nullable: false),
+                    PriceBrutto = table.Column<SqlMoney>(type: "money", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Factures", x => x.IdFacture);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    IdNews = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LinkTitle = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(MAX)", nullable: true),
+                    Position = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.IdNews);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Page",
+                columns: table => new
+                {
+                    IdPage = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LinkTitle = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(MAX)", nullable: true),
+                    Pozition = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Page", x => x.IdPage);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    IdOfProduct = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CodeOfProduct = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NameOfProduct = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    ProductDescription = table.Column<string>(type: "nvarchar(MAX)", nullable: true),
+                    Price = table.Column<SqlMoney>(type: "money", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.IdOfProduct);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductType",
+                columns: table => new
+                {
+                    IdProductType = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductTypeName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    ProductTypeSubName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(MAX)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductType", x => x.IdProductType);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    IdUsers = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserSurname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<EmailAddressAttribute>(type: "email", nullable: true),
+                    WorkPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Stanowisko = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.IdUsers);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Factures");
+
+            migrationBuilder.DropTable(
+                name: "News");
+
+            migrationBuilder.DropTable(
+                name: "Page");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "ProductType");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- SklepPortalWWW/Program.cs
+++ SklepPortalWWW/Program.cs
@@ -1,27 +1,41 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using SklepData.Data;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+namespace SklepPortalWWW;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddDbContext<SklepContext>(options =>
+           options.UseSqlServer(builder.Configuration.GetConnectionString("SklepContext") ?? throw new InvalidOperationException("Connection string 'SklepContext' not found.")));
+
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
